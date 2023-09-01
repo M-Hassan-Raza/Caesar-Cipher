15bea59d573f61cb6e17a45cef7b9144a888e018
Muhammad Hassan Raza <h.raza1164@yahoo.com>
2023-09-01 12:12:32 +0500

    added main function, added error checks

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 """This module contains the main function for the program."""
 
+
 def main():
     """Main function for the program."""
-    
+    plaintext = input("Enter the plaintext to encrypt")
+    key = input("Enter the key of the cipher")
+
+    if plaintext is None or plaintext == "":
+        return "Empty string can't be encoded"
+    if (key is None) or (key == 0) or (not isinstance(key, int)):
+        return "Empty key can't be encoded"
+
+    return calculate_cipher(plaintext, key)
+
 
 def calculate_cipher(plaintext_string, key):
-    """Takes in a key and a string, returns the cipher """
+    """Takes in a key and a string, returns the cipher"""
     cipher = ""
-    plaintext_string = plaintext_string.strip(" ")
     for char in plaintext_string:
+        if char == " ":
+            cipher += " "
+        elif (ord(char) < 65 or ord(char) <= 90) or (
+            ord(char) < 97 or ord(char) <= 122
+        ):
+            cipher += chr((ord(char) - 65 + key) % 26 + 65)
         cipher += chr((ord(char) - 97 + key) % 26 + 97)
 
     return cipher
